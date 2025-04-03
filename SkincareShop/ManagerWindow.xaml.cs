8b3d9b804c2485f1aa10c16aabed2422d21d8c38
thienvip1072004@gmail.com <thienvip1072004@gmail.com>
2025-04-03 12:45:59 +0700

    Add Manager Window and Order Window

---
+++ SkincareShop/ManagerWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SkincareShop
+{
+    /// <summary>
+    /// Interaction logic for ManagerWindow.xaml
+    /// </summary>
+    public partial class ManagerWindow : Window
+    {
+        private User _currentUser;
+        public ManagerWindow(User currentUser)
+        {
+            InitializeComponent();
+            DisplayWelcomeMessage();
+            _currentUser = currentUser;
+        }
+
+        private void DisplayWelcomeMessage()
+        {
+            if (_currentUser != null)
+            {
+                WelcomeTextBlock.Text = $"Xin chào {_currentUser.FullName}";
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DisplayWelcomeMessage();
+        }
+
+        private void ManageOrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderWindow orderWindow = new OrderWindow(_currentUser);
+            orderWindow.Show();
+            this.Close();
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
+        }
+    }
+}

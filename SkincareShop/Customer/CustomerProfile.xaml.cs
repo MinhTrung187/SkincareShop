101e81bd52385ae2d73bdf3bbde14e826d9e088c
Vũ Minh Đức <144935378+DumbMies@users.noreply.github.com>
2025-04-03 14:54:28 +0700

    fix showing orderHistory

--- SkincareShop/Customer/CustomerProfile.xaml.cs
+++ SkincareShop/Customer/CustomerProfile.xaml.cs
@@ -5,7 +5,9 @@ using SkincareShop.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,45 +24,86 @@ namespace SkincareShop.Customer
     /// <summary>
     /// Interaction logic for CustomerProfile.xaml
     /// </summary>
-    public partial class CustomerProfile : Window
+    public partial class CustomerProfile : Window, INotifyPropertyChanged
     {
-        private readonly UserService _Userservice;
+        private readonly UserService _userService;
+        private readonly OrderService _orderService;
         private readonly int _userId;
-        private readonly OrderService orderService;
+
+        private string _fullName;
+        private string _email;
+        private string _createdAt;
+
+        public ObservableCollection<OrderHistoryItem> OrderHistory { get; set; }
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                _fullName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CreatedAt
+        {
+            get => _createdAt;
+            set
+            {
+                _createdAt = value;
+                OnPropertyChanged();
+            }
+        }
 
         public CustomerProfile(int id)
         {
             InitializeComponent();
             _userId = id;
-            _Userservice = new UserService();
-            orderService = new OrderService();
-
+            _userService = new UserService();
+            _orderService = new OrderService();
+            OrderHistory = new ObservableCollection<OrderHistoryItem>();
+            DataContext = this;
+            LoadCustomerData();
+            LoadOrderHistory();
         }
+
         private void LoadCustomerData()
         {
-            var user = _Userservice.GetUserById(_userId);
-            txtFullName.Text = user.FullName;
-            txtEmail.Text = user.Email;
-            txtCreatedAt.Text=user.CreatedAt.ToString();
-
-            //DataContext = user;
+            var user = _userService.GetUserById(_userId);
+            FullName = user.FullName;
+            Email = user.Email;
+            CreatedAt = user.CreatedAt?.ToString();
         }
+
         private void LoadOrderHistory()
         {
-            List<Order>orders =orderService.GetOrderByUserId(_userId);
-            DataContext = orders;
+            var orders = _orderService.GetOrderHistory(_userId);
+            OrderHistory.Clear();
+            foreach (var order in orders)
+            {
+                OrderHistory.Add(order);
+            }
         }
 
         private void SaveProfile_Click(object sender, RoutedEventArgs e)
         {
-
+            // Save profile changes logic here
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            LoadCustomerData();
-            LoadOrderHistory();
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
